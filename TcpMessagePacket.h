7f65e32ad969ebe894d7212fa37b5e4e94d5b897
kikuke <kkkjs0814@naver.com>
2023-02-11 01:39:30 +0900

    [Add] Error Catch

--- TcpMessagePacket.h
+++ TcpMessagePacket.h
@@ -7,44 +7,19 @@
 class TcpMessagePacket : public PacketHandler
 {
 private:
-    int MessageEcho(MessageEchoData meData);
+    int MessageEcho(int sock, MessageEchoData meData);
 
+    Logger* log;
 public:
-    TcpMessagePacket():PacketHandler(MESSAGE) {}
-    ~TcpMessagePacket() {}
-
-    int execute(int sock, unsigned int subOp, RingBuffer& buffer) override
-    {
-        switch (subOp)
-        {
-        case MESSAGE_ECHO:
-            MessageEchoData meData;
-            if(DequeueData(meData, buffer) != TCP_PACKET_END_CODE){
-                return 0;//에러코드로 바꿔주기
-            }
-
-            return MessageEcho(meData);//에러나면 에러코드가 반환됨.
-            break;
-        
-        default:
-            //error
-            return 0;
-            break;
-        }
-        return 1;
+    TcpMessagePacket():PacketHandler(MESSAGE) {
+        log = new Logger("TcpMessagePacket");
     }
 
-    int catchError(int sock, unsigned int errorCode) override
-    {
-        switch (errorCode)
-        {
-        default:
-            //error
-            return 0;
-            break;
-        }
-        return 1;
+    ~TcpMessagePacket() {
+        delete log;
     }
+    int execute(int sock, unsigned int subOp, RingBuffer& buffer) override;
+    int catchError(int sock, unsigned int errorCode) override;
 };
 
 #endif
